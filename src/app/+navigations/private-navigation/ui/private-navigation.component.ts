33812e99583a2839ba51fe6af1fb4a88ec4b57ee
MrMani24 <manibaranriz8@gmail.com>
2025-06-02 14:38:30 +0330

    login

--- src/app/+navigations/private-navigation/ui/private-navigation.component.ts
+++ src/app/+navigations/private-navigation/ui/private-navigation.component.ts
@@ -1,15 +1,36 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-private-navigation',
-  imports: [RouterOutlet , RouterLink],
   templateUrl: './private-navigation.component.html',
-  styleUrl: './private-navigation.component.scss'
+  styleUrl: './private-navigation.component.scss',
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+    RouterOutlet,
+    RouterLink,
+  ]
 })
 export class PrivateNavigationComponent {
-  router= inject(Router)
-  singout(){
-    this.router.navigateByUrl('Login');
-  }
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
--- src/app/+pages/+public/login/ui/login.component.ts
+++ src/app/+pages/+public/login/ui/login.component.ts
@@ -1,16 +1,63 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-
+import { MatCardModule } from '@angular/material/card';
+import { map, Observable, shareReplay } from 'rxjs';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { login } from '../models/login';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    MatCardModule,
+    AsyncPipe,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   router = inject(Router);
-  login() {
+  b1: boolean = false;
+  b2: boolean = false;
+  b3: boolean = false;
+  login: login = { username: '', password: '', keepme: false }
+  check() {
+    if (this.login.username.trim() === 'admin' && this.login.password.trim() === '12345678') {
+      //برای در نظر نگرفتن فاصله های کنار مقدار وارد شده trim()
+      this.b3 = true;
+      setTimeout(()=>{
+        this.go();
+        console.log(this.login);
+      },2500);
+    }
+    if (this.login.username != '12345678') {
+      this.b2 = true;
+    }
+    if (this.login.username != 'admin') {
+      this.b1 = true;
+    }
+  }
+  go() {
     this.router.navigateByUrl('/admin');
   }
-
+  private breakpointObserver = inject(BreakpointObserver);
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 }
